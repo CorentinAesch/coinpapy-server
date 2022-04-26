efc3ffc251eac596513dfc3473d3e302667c6f1a
Corentin Aesch <corentin.aesch@gmail.com>
2022-04-26 19:18:23 +0100

    Watchlist Routes done

--- routes/watchlist.routes.js
+++ routes/watchlist.routes.js
@@ -41,19 +41,25 @@ router.post("/watchlist", isAuthenticated, async (req, res) => {
 
         let user = await User.findById(userId).populate("watchlist");
         
-        user.watchlist.forEach(coin => {
-            if (user.watchlist.includes(coin)){
-                res.status(400).json({ message: "This coin is already in your watchlist" });
-            }
+        const isFav = user.watchlist.filter((favcoin) => {
+            return favcoin.coin.toString() === coin;
         });
-
-        const newFav = await Favcoin.create({ coin, variationAlert, priceAlert });
-        user = await User.findByIdAndUpdate(userId, {$push: { watchlist: newFav  }}, { new: true});
-
-        const newNotif = await Notification.create({ coin, userId });
-        user = await User.findByIdAndUpdate(userId, {$push: { notifications: newNotif  }}, { new: true});
         
-        res.status(200).json({ newFav, newNotif });
+        if (isFav.length){
+
+            res.status(400).json({ message: "This coin is already in your watchlist" });
+
+        } else {
+
+            const newFav = await Favcoin.create({ coin, variationAlert, priceAlert });
+            user = await User.findByIdAndUpdate(userId, {$push: { watchlist: newFav  }}, { new: true});
+
+            const newNotif = await Notification.create({ coin, userId });
+            user = await User.findByIdAndUpdate(userId, {$push: { notifications: newNotif  }}, { new: true});
+    
+            res.status(200).json({ newFav, newNotif });
+        }
+        
 
     }catch(e){
         res.status(500).json({ message: e });
@@ -62,17 +68,11 @@ router.post("/watchlist", isAuthenticated, async (req, res) => {
 
 
 router.get('/watchlist/:favcoinId', isAuthenticated, async (req, res) => {
-
-    
+    const favcoinId = req.params;
     
     try {
-
-        
-
-        // TODO
-
-
-
+        const favCoin = Favcoin.findById({_id: favcoinId}).populate("coin");
+        res.status(200).json({ favCoin })
         
     } catch(e) {
         res.status(500).json({ message: e });
